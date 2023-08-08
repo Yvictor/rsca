7c36628d5ca4db78f1f9a25ad305cde10a6fc794
yvictor <yvictor3141@gmail.com>
2023-08-08 12:31:44 +0800

    feat: sign

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
-use openssl::pkcs12::{Pkcs12, ParsedPkcs12_2};
-
-pub struct TWCA {
-
-}
+use openssl::pkcs12::{ParsedPkcs12_2, Pkcs12};
+use openssl::pkcs7::{Pkcs7, Pkcs7Flags};
+pub struct TWCA {}
 
 pub fn load_cert(der: &[u8], password: &str) -> Option<ParsedPkcs12_2> {
     let res = Pkcs12::from_der(&der);
@@ -13,13 +11,32 @@ pub fn load_cert(der: &[u8], password: &str) -> Option<ParsedPkcs12_2> {
                 Ok(parsed) => {
                     Some(parsed)
                     // println!("Parsed: {:?}", parsed);
-                },
+                }
                 Err(e) => {
                     println!("Error: {}", e);
                     None
                 }
             }
-        },
+        }
+        Err(e) => {
+            println!("Error: {}", e);
+            None
+        }
+    }
+}
+
+pub fn sign(pkcs12: ParsedPkcs12_2, data: &[u8]) -> Option<Pkcs7> {
+    let sign = Pkcs7::sign(
+        &pkcs12.cert.unwrap(),
+        &pkcs12.pkey.unwrap(),
+        &pkcs12.ca.unwrap(),
+        &data,
+        Pkcs7Flags::BINARY,
+    );
+    match sign {
+        Ok(signed) => {
+            Some(signed)
+        }
         Err(e) => {
             println!("Error: {}", e);
             None
@@ -40,7 +57,10 @@ mod tests {
         let der = std::fs::read("Sinopac.pfx").unwrap();
         let password = "";
         let cert = load_cert(&der, &password);
-        assert!(cert.is_some());
-        assert!(cert.unwrap().pkey.is_some());
+        // assert!(cert.is_some());
+        // assert!(cert.unwrap().pkey.is_some());
+        let data = b"1234567890";
+        let sign_data = sign(cert.unwrap(), data);
+        assert!(sign_data.is_some());
     }
 }
