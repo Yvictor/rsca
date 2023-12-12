8db514c8ff30d1bc4623348017b50ebe46b095df
Yvictor <yvictor3141@gmail.com>
2023-12-13 04:51:02 +0800

    feat: bind TWCA

---
+++ pyrsca/pyrsca/pyrsca.py
@@ -0,0 +1,11 @@
+class PyTWCA:
+    """
+    PyTWCA
+
+    """
+
+    def __init__(self, path: str, password: str) -> None:
+        ...
+
+    def get_person_id(self) -> str:
+        """get person_id of ca"""
--- pyrsca/src/lib.rs
+++ pyrsca/src/lib.rs
@@ -1,20 +1,41 @@
 use pyo3::prelude::*;
+use pyo3::types::PyBytes;
 
+#[pyclass]
+struct PyTWCA {
+    twca: rsca::TWCA
+}
+
+#[pymethods]
+impl PyTWCA {
+    #[new]
+    fn new(path: &str, password: &str) -> Self {
+        let twca = rsca::TWCA::new(path, password).unwrap();
+        PyTWCA { twca }
+    }
+
+    fn get_person_id(&self) -> PyResult<String> {
+        Ok(self.twca.get_common_name().unwrap())
+    }
+}
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize, password: &str) -> PyResult<String> {
-    let der = std::fs::read("Sinopac.pfx").unwrap();
+fn sign(path: &str, password: &str) -> PyResult<String> {
+    let der = std::fs::read(path).unwrap();
     let cert = rsca::load_cert(&der, password).unwrap();
     let data = b"1234567890";
-    let _sign_data = rsca::sign(cert, data).unwrap();
+    let sign_data = rsca::sign(cert, data).unwrap();
     // sign_data
-    Ok((a + b).to_string())
+    // let signed = sign_data.to().unwrap();
+    // PyBytes::new(py, &signed).into()
+    Ok(sign_data)
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pyrsca(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(sign, m)?)?;
+    m.add_class::<PyTWCA>()?;
     Ok(())
 }
